880596e580b5ef3264113ac5ae546aec44bfbaf2
KrIsKa7a <kristiqn.ivanov@abv.bg>
2017-12-10 23:08:23 +0200

    Added solution

---
+++ BookLibrary/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<BookLibrary.Models.Book> Books { get; set; }
+    }
+}
